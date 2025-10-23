31416b704872a94c919f2b709306ee57d804731a
Corno Schraverus <corno@schraverus.com>
2025-10-23 20:51:36 +0200

    added test dir

--- pub/src/test.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import * as _eb from 'exupery-core-bin'
-
-import { $$ } from "./procedures/test"
-
-_eb.run_unguaranteed_main_procedure($$)
---
+++ test/src/bin/test.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import * as _eb from 'exupery-core-bin'
+import * as _ed from 'exupery-core-dev'
+
+_ed.log_debug_message("TEST NOT IMPLEMENTED YET", () => {})
