7348bd10e857a5631b26e8995c20d09912c8b81e
Corno Schraverus <corno@schraverus.com>
2025-12-28 09:19:39 +0100

    ...

--- pub/src/interface/generated/pareto/core/resolve.ts
+++ pub/src/interface/generated/pareto/core/resolve.ts
@@ -9,12 +9,13 @@ export type Non_Circular_Result<T> =
     | ['error', ['circular', _et.List<string>]]
     | ['resolved', T]
 
-export type Acyclic_Lookup<T> = _et.Optional_Value<_et.Lookup<Non_Circular_Result<T>>> //FIXME should this not be optional?
+export type Lookup<T> = { get_entry: (key: string) => _et.Optional_Value<T> }
+export type Acyclic_Lookup<T> = _et.Optional_Value<Lookup<Non_Circular_Result<T>>> //FIXME should this not be optional?
 export type _T_Acyclic_Lookup<T> = Acyclic_Lookup<T>
 
 export type Possibly_Circular_Result<T> = _et.Circular_Dependency<T>
 
-export type Cyclic_Lookup<T> = _et.Optional_Value<_et.Lookup<Possibly_Circular_Result<T>>> //FIXME should this not be optional?
+export type Cyclic_Lookup<T> = _et.Optional_Value<Lookup<Possibly_Circular_Result<T>>> //FIXME should this not be optional?
 export type _T_Cyclic_Lookup<T> = Cyclic_Lookup<T>
 
 export type Lookup_Stack<T> = _et.List<Acyclic_Lookup<T>>
