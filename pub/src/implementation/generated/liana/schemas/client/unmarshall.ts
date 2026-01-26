cc4a3e0bc7bdbc7ba44b1d499ecd2b8ec099fe92
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/client/unmarshall.ts
@@ -0,0 +1,13 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/client/unmarshall"
+export const Position: t_signatures.Position = ($,) => _p.unreachable_code_path()
+export const Range: t_signatures.Range = ($,) => _p.unreachable_code_path()
+export const Format_Options: t_signatures.Format_Options = ($,) => _p.unreachable_code_path()
+export const Replace: t_signatures.Replace = ($,) => _p.unreachable_code_path()
+export const Text_Edit: t_signatures.Text_Edit = ($,) => _p.unreachable_code_path()
+export const Format_Result: t_signatures.Format_Result = ($,) => _p.unreachable_code_path()
+export const Format_Error: t_signatures.Format_Error = ($,) => _p.unreachable_code_path()
+export const Format_Parameters: t_signatures.Format_Parameters = ($,) => _p.unreachable_code_path()
+export const Sort_Alphabetically_Parameters: t_signatures.Sort_Alphabetically_Parameters = ($,) => _p.unreachable_code_path()
---
+++ pub/src/implementation/generated/liana/schemas/server/unmarshall.ts
@@ -0,0 +1,22 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/server/unmarshall"
+export const Diagnostic_Severity: t_signatures.Diagnostic_Severity = ($,) => _p.unreachable_code_path()
+export const Position: t_signatures.Position = ($,) => _p.unreachable_code_path()
+export const Range: t_signatures.Range = ($,) => _p.unreachable_code_path()
+export const Diagnostic: t_signatures.Diagnostic = ($,) => _p.unreachable_code_path()
+export const Diagnostics: t_signatures.Diagnostics = ($,) => _p.unreachable_code_path()
+export const Document_Data: t_signatures.Document_Data = ($,) => _p.unreachable_code_path()
+export const Hover_Texts: t_signatures.Hover_Texts = ($,) => _p.unreachable_code_path()
+export const Optional_Hover_Texts: t_signatures.Optional_Hover_Texts = ($,) => _p.unreachable_code_path()
+export const On_Hover_Result: t_signatures.On_Hover_Result = ($,) => _p.unreachable_code_path()
+export const Completion_Items: t_signatures.Completion_Items = ($,) => _p.unreachable_code_path()
+export const Optional_Completion_Items: t_signatures.Optional_Completion_Items = ($,) => _p.unreachable_code_path()
+export const On_Completion_Result: t_signatures.On_Completion_Result = ($,) => _p.unreachable_code_path()
+export const On_Validate_Document_Result: t_signatures.On_Validate_Document_Result = ($,) => _p.unreachable_code_path()
+export const Convert_To_JSON_Parameters: t_signatures.Convert_To_JSON_Parameters = ($,) => _p.unreachable_code_path()
+export const Seal_Parameters: t_signatures.Seal_Parameters = ($,) => _p.unreachable_code_path()
+export const On_Completion_Parameters: t_signatures.On_Completion_Parameters = ($,) => _p.unreachable_code_path()
+export const On_Hover_Parameters: t_signatures.On_Hover_Parameters = ($,) => _p.unreachable_code_path()
+export const Validate_Document_Parameters: t_signatures.Validate_Document_Parameters = ($,) => _p.unreachable_code_path()
